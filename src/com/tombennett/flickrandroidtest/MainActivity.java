2999434eaba8ac47b8bcb82976fa3aaa6a630eaa
Thomas Bennett <thomas.j.bennett@gmail.com>
2014-04-17 18:47:01 -0700

    Don't download photo data during onstart

--- src/com/tombennett/flickrandroidtest/MainActivity.java
+++ src/com/tombennett/flickrandroidtest/MainActivity.java
@@ -21,11 +21,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFlickr = new Flickr(Constants.FLICKR_API_KEY);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
         new GetInterestingPhotosClass().execute();
     }
 
